798db74aceacd57ad9e7c81eaeeb0a3899d2e9c4
DouglasRodr <wandersondouglasr@gmail.com>
2021-09-18 20:56:10 -0300

    Aula 3

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,23 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import logo from '../../svg/logo.svg';
+
 const Header = () => {
     return (
-        <h1>Header.js</h1>
+        <>
+            <header className="flex-space-between">
+                <div className="logo">
+                    <img src={logo} alt="Logo"/>
+                </div>
+                <div className="search">
+                    <input type="text" name="search" className="input-search"></input>
+                </div>
+                <ul className="menu">
+                    <li><a href="#" className="nav-link">Categories</a></li>
+                    <li><a href="#" className="nav-link">About</a></li>
+                    <li><a href="#" className="nav-link">Contact</a></li>
+                </ul>
+            </header>
+        </>
     );
 }
 
