7a21e848d79b7237618700cc255ce5b50940a9b1
DouglasRodr <wandersondouglasr@gmail.com>
2021-09-18 20:16:44 -0300

    Aula 2 Parte 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import Header from './components/Header/index';
+import Article from './components/Article/index';
+
 function App() {
   return (
-    <h1>Meu Blog</h1>
+    <>
+      <Header></Header>
+      <Article></Article>
+      <h1>Meu Blog</h1>
+    </>
   );
 }
 
---
+++ src/components/Article/index.js
@@ -0,0 +1,12 @@
+import Post from '../Post/index';
+
+const Article = () => {
+    return (
+        <>
+            <h1>Article.js</h1>
+            <Post/>
+        </>        
+    );
+}
+
+export default Article;
